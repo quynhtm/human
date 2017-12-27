423eb77ec8ba508de2b9b856481b88934189d21d
quynhtm <manhquynh1984@gmail.com>
2017-12-27 23:22:24 +0700

    sửa

--- app/Http/Controllers/Admin/AdminDashBoardController.php
+++ app/Http/Controllers/Admin/AdminDashBoardController.php
@@ -9,7 +9,6 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Support\Facades\Request;
 use App\Http\Controllers\BaseAdminController;
-use App\Http\Models\SystemSetting;
 use Illuminate\Support\Facades\Session;
 use App\Library\AdminFunction\FunctionLib;
 use App\Library\AdminFunction\Define;
@@ -22,18 +21,7 @@ class AdminDashBoardController extends BaseAdminController{
 
     public function dashboard(){
         $total = 0;
-        $data = SystemSetting::searchByCondition(array("field_get"=>"system_setting_id,system_content,system_content_en"),1,0,$total);
-        if (count($data)>0){
-            if ($this->languageSite == Define::VIETNAM_LANGUAGE){
-                $data['content'] = $data[0]['system_content'];
-            }else{
-                $data['content'] = $data[0]['system_content_en'];
-            }
-            $id = $data[0]['system_setting_id'];
-        }else{
-            $data = array();
-            $id=0;
-        }
+        $data = array();
 
         return view('admin.AdminDashBoard.index',[
             'user'=>$this->user,
