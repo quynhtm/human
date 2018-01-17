9959649604205fdd03cef3e6f81abf26f9fbb63f
quynhtm <manhquynh1984@gmail.com>
2018-01-18 01:08:30 +0700

    commit sửa

--- routes/hr.php
+++ routes/hr.php
@@ -5,3 +5,7 @@ Route::match(['GET','POST'],'department/view', array('as' => 'hr.departmentView'
 Route::get('department/edit/{id?}', array('as' => 'hr.departmentEdit','uses' => HResources.'\HrDepartmentController@getItem'));
 Route::post('department/edit/{id?}', array('as' => 'hr.departmentEdit','uses' => HResources.'\HrDepartmentController@postItem'));
 Route::post('department/deleteMenu', array('as' => 'hr.deleteDepartment','uses' => HResources.'\HrDepartmentController@deleteMenu'));//ajax
+
+
+/*thông tin Nhân sự */
+Route::match(['GET','POST'],'personnel/view', array('as' => 'hr.personnelView','uses' => HResources.'\HrPersonnelController@view'));
