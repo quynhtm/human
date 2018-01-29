589ecd009e62a978e6ba4b60836415266a7b9cc1
quynhtm <manhquynh1984@gmail.com>
2018-01-29 21:04:04 +0700

    commit sửa

---
+++ app/Console/Commands/resetUser.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * QuynhTM:add
+ */
+
+namespace App\Console\Commands;
+use Illuminate\Console\Command;
+
+use App\Library\AdminFunction\Define;
+use Illuminate\Support\Facades\DB;
+use App\Http\Models\Hr\Person;
+
+class resetUser extends Command{
+    protected $signature = 'resetUser';
+    protected $description = 'Reset User';
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function handle()
+    {
+        $dataUpdate['user_last_login'] = time();
+        $dataUpdate['user_edit_name'] = 'cronjob';
+        $data = DB::table(Define::TABLE_USER)
+            ->where('user_status', '=', Define::STATUS_SHOW)
+            ->get(array('user_id'));
+        if($data){
+            foreach ($data as $k=>$user){
+                Person::updateItem($user->user_id,$dataUpdate);
+            }
+            echo 'Co tong: '.count($data).' da cap nhat xong';
+        }
+    }
+}
