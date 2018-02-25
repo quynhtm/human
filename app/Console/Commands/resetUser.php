5d22d1809fd5c209927d343b9530b096e595dafb
quynhtm <manhquynh1984@gmail.com>
2018-02-26 00:26:20 +0700

    commit sửa

--- app/Console/Commands/resetUser.php
+++ app/Console/Commands/resetUser.php
@@ -9,6 +9,7 @@ use Illuminate\Console\Command;
 use App\Library\AdminFunction\Define;
 use Illuminate\Support\Facades\DB;
 use App\Http\Models\Hr\Person;
+use App\Http\Models\Admin\User;
 use App\Mail\MailSystem;
 use Illuminate\Support\Facades\Mail;
 
@@ -21,18 +22,19 @@ class resetUser extends Command{
     }
     public function handle()
     {
-        Mail::to('manhquynh1984@gmail.com')->send(new MailSystem());
+        //Mail::to('nguyenduypt86@gmail.com')->send(new MailSystem());
+        //Mail::to('manhquynh1984@gmail.com')->send(new MailSystem());
 
-        /*$dataUpdate['user_last_login'] = time();
+        $dataUpdate['user_last_login'] = time();
         $dataUpdate['user_edit_name'] = 'cronjob';
         $data = DB::table(Define::TABLE_USER)
             ->where('user_status', '=', Define::STATUS_SHOW)
             ->get(array('user_id'));
         if($data){
             foreach ($data as $k=>$user){
-                Person::updateItem($user->user_id,$dataUpdate);
+                User::updateUser($user->user_id,$dataUpdate);
             }
             echo 'Co tong: '.count($data).' da cap nhat xong';
-        }*/
+        }
     }
 }
