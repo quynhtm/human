a5f1589f8fefe32877ed132911b8be04bcf29968
nguyenduypt86 <nguyenduypt86@gmail.com>
2018-05-02 13:03:02 +0700

    Duy

--- app/Http/Controllers/Hr/QuitJobController.php
+++ app/Http/Controllers/Hr/QuitJobController.php
@@ -55,7 +55,7 @@ class QuitJobController extends BaseAdminController
 
     public function getQuitJob($person_ids)
     {
-        echo 'abc';die;
+
         CGlobal::$pageAdminTitle = 'Thiết lập buộc thôi việc nhân sự';
         $person_id = FunctionLib::outputId($person_ids);
         if (!$this->is_root && !in_array($this->permission_full, $this->permission) && !in_array($this->permission_edit, $this->permission) && !in_array($this->permission_create, $this->permission)) {
@@ -66,6 +66,11 @@ class QuitJobController extends BaseAdminController
             $data = QuitJob::getQuitJobByPersonId($person_id,Define::QUITJOB_THOI_VIEC);
         }
 
+        $quit_job_id = 0;
+        if(sizeof($data) > 0){
+            $quit_job_id = $data->quit_job_id;
+        }
+
         //thong tin nhan sự
         $infoPerson = Person::getPersonById($person_id);
         $this->getDataDefault();
@@ -74,12 +79,15 @@ class QuitJobController extends BaseAdminController
             'data' => $data,
             'infoPerson' => $infoPerson,
             'person_id' => $person_id,
+            'quit_job_id' => $quit_job_id,
         ], $this->viewPermission));
     }
     public function postQuitJob($person_ids)
     {
         CGlobal::$pageAdminTitle = 'Thiết lập buộc thôi việc nhân sự';
         $person_id = FunctionLib::outputId($person_ids);
+        $id_hiden = Request::get('id_hiden', '');
+
         if (!$this->is_root && !in_array($this->permission_full, $this->permission) && !in_array($this->permission_edit, $this->permission) && !in_array($this->permission_create, $this->permission)) {
             return Redirect::route('admin.dashboard', array('error' => Define::ERROR_PERMISSION));
         }
@@ -87,16 +95,16 @@ class QuitJobController extends BaseAdminController
 
         if ($this->valid($data) && empty($this->error)) {
             $passport = QuitJob::getQuitJobByPersonId($person_id,Define::QUITJOB_THOI_VIEC);
-            $id = ($passport && isset($passport->quit_job_id)) ? $passport->quit_job_id : 0;
+            $id = ($passport && isset($passport->quit_job_id)) ? $passport->quit_job_id : FunctionLib::outputId($id_hiden);
             $data['quit_job_date_creater'] = ($data['quit_job_date_creater'] != '')? strtotime($data['quit_job_date_creater']): '';
             if ($id > 0) {
                 //cap nhat
+                $data['quit_job_type'] = Define::QUITJOB_THOI_VIEC;
                 if (QuitJob::updateItem($id, $data)) {
                     return Redirect::route('hr.personnelView');
                 }
             } else {
                 //them moi
-                //FunctionLib::debug($data);
                 $data['quit_job_person_id'] = $person_id;
                 $data['quit_job_type'] = Define::QUITJOB_THOI_VIEC;
                 if (QuitJob::createItem($data)) {
@@ -104,6 +112,15 @@ class QuitJobController extends BaseAdminController
                 }
             }
         }
+
+        $quit_job_id = 0;
+        if($person_id > 0){
+            $dataQuitJob = Retirement::getRetirementByPersonId($person_id);
+            if(sizeof($dataQuitJob) > 0){
+                $quit_job_id = $dataQuitJob->quit_job_id;
+            }
+        }
+
         //thong tin nhan sự
         $infoPerson = Person::getPersonById($person_id);
         $this->getDataDefault();
@@ -113,6 +130,7 @@ class QuitJobController extends BaseAdminController
             'person_id' => $person_id,
             'infoPerson' => $infoPerson,
             'error' => $this->error,
+            'quit_job_id' => $quit_job_id,
         ], $this->viewPermission));
     }
 
@@ -148,6 +166,8 @@ class QuitJobController extends BaseAdminController
     {
         CGlobal::$pageAdminTitle = 'Thiết lập Nghỉ việc / Chuyển công tác nhân sự';
         $person_id = FunctionLib::outputId($person_ids);
+        $id_hiden = Request::get('id_hiden', '');
+
         if (!$this->is_root && !in_array($this->permission_full, $this->permission) && !in_array($this->permission_edit, $this->permission) && !in_array($this->permission_create, $this->permission)) {
             return Redirect::route('admin.dashboard', array('error' => Define::ERROR_PERMISSION));
         }
@@ -155,10 +175,11 @@ class QuitJobController extends BaseAdminController
 
         if ($this->valid($data) && empty($this->error)) {
             $passport = QuitJob::getQuitJobByPersonId($person_id,Define::QUITJOB_CHUYEN_CONGTAC);
-            $id = ($passport && isset($passport->quit_job_id)) ? $passport->quit_job_id : 0;
+            $id = ($passport && isset($passport->quit_job_id)) ? $passport->quit_job_id : FunctionLib::outputId($id_hiden);;
             $data['quit_job_date_creater'] = ($data['quit_job_date_creater'] != '')? strtotime($data['quit_job_date_creater']): '';
             if ($id > 0) {
                 //cap nhat
+                $data['quit_job_type'] = Define::QUITJOB_CHUYEN_CONGTAC;
                 if (QuitJob::updateItem($id, $data)) {
                     return Redirect::route('hr.personnelView');
                 }
